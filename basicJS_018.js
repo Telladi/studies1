f0b4f0c54b323433b3ab8c4ff5634859eb593888
Telladi <telladirtv@mail.ru>
2023-03-07 21:49:19 +0300

    basicJS_018

---
+++ basicJS_018.js
@@ -0,0 +1,12 @@
+const myArray = [1, 2 , 3 , 4]
+console.log(myArray)
+console.log(myArray.length)
+
+myArray.push('abc')
+
+console.log(myArray)
+
+myArray.push(true)
+
+console.log(myArray)
+console.log(myArray.length)
