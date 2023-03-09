3daec521914060951bafad3d6cc088d95ced91b9
Telladi <telladirtv@mail.ru>
2023-03-09 21:08:29 +0300

    sumPositiveNumbers

---
+++ sumPositiveNumbers.js
@@ -0,0 +1,11 @@
+const sumPositiveNumbers = (a, b) => {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return 'One of the arguments is not a number'
+    }
+
+    if (a <= 0 || b <= 0) {
+        return "Numbers are not positive"
+    }
+
+    return a + b
+}
