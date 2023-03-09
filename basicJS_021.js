a566b69edee5a8df3a4dedce7c0399afd8cadec3
Telladi <telladirtv@mail.ru>
2023-03-09 20:55:41 +0300

    basicJS_022

---
+++ basicJS_021.js
@@ -0,0 +1,12 @@
+const age = 24;
+if(age >= 18) {
+    console.log('Is adult')
+}
+
+if (age >= 12 && age < 18) {
+    console.log('Is teenager')
+}
+
+if (age < 12) {
+    console.log('Is child')
+}
